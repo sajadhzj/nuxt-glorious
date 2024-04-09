7d96342e2a43c5e4919f9c5e8e43c70e683eeca5
unibas.official <unibas.official@gmail.com>
2024-04-09 08:41:06 +0330

    chore(release): v0.0.10

--- src/runtime/middlewares/g-auth.ts
+++
@@ -1,8 +0,0 @@
-import {navigateTo, useRuntimeConfig} from "nuxt/app";
-
-export default defineNuxtRouteMiddleware(async (to: any, from: any) => {
-  const gs = GloriousStore()
-  const moduleConfig: any = useRuntimeConfig()
-  if (!gs.authIsLogin())
-    return navigateTo(moduleConfig.public.glorious.auth.redirect.logout)
-})
