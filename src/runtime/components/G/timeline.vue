edbbe9b9eeb178c3e6d84daa9781feb34b794cc5
sajad <sajad.hzj@gmail.com>
2024-11-12 14:25:44 +0330

    component timeline

---
+++ src/runtime/components/G/timeline.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+const props = defineProps({
+  items: {
+    required: true,
+  },
+  color: {
+    required: false,
+    default: 'green',
+    type: String as () => 'orange' | 'blue' | 'gray' | 'red' | 'green',
+  },
+})
+</script>
+<template>
+  <div class="flex gap-3 mr-4">
+    <div
+      class="flex flex-col gap-5 glorious-timeline-items"
+      :class="[`color-${props.color}`]"
+    >
+      <div
+        v-for="(item, index) in props.items.reverse()"
+        :key="index"
+      >
+        <div class="glorious-timeline-items-count">
+          {{ props.items.length - index }}
+        </div>
+        <slot
+          name="item"
+          :item="item"
+        >
+        </slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import '../../assets/style/components/timeline.scss';
+</style>
