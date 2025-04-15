d6971fc0a74f4e30227ae9fc338469f7e8ab87a2
sajad <sajad.hzj@gmail.com>
2025-04-15 19:36:57 +0330

    animation for error text

--- src/runtime/components/G/ErrorText.vue
+++ src/runtime/components/G/ErrorText.vue
@@ -7,10 +7,33 @@ const gs = GloriousStore()
 const error: any = props.error.split('|')
 </script>
 <template>
+<<<<<<< Updated upstream
   <span
     v-if="gs.forms[error[0]]?.errors[error[1]]"
     class="validation-error-text text-red-500 text-sm"
   >
     {{ gs.forms[error[0]].errors[error[1]][0] }}
   </span>
+=======
+  <div>
+    <span
+      v-if="gs.forms[error[0]]?.errors[error[1]] && !isAll()"
+      class="validation-error-text text-red-500 text-sm"
+    >
+      {{ gs.forms[error[0]].errors[error[1]][0] }}
+    </span>
+    <ul
+      v-else-if="isAll()"
+      class="flex flex-col list-disc ltr:ml-4 rtl:mr-4"
+    >
+      <li
+        v-for="(item, index) in gs.forms[error[0]].errors"
+        :key="index"
+        class="text-red-500 text-sm"
+      >
+        <span class="validation-error-text">{{ item[0] }}</span>
+      </li>
+    </ul>
+  </div>
+>>>>>>> Stashed changes
 </template>
